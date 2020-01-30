ef82c0e06df67535cfe79d257da823bdb7113703
alexalikiotis <alexalikiotis5@gmail.com>
2020-01-31 00:04:50 +0200

    Updating examples

--- examples/server-02/app.js
+++ examples/server-02/app.js
@@ -1,19 +1,29 @@
 const fs = require('fs');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
 
+// index.html
 app.get('/', (req, res) => {
   // create read stream
   const stream = fs.createReadStream('./index.html');
-
   // handle stream error
   stream.on('error', (err) => {
     res.status(400).json({ error: err.message });
   });
-
   // pipe stream data to response
   stream.pipe(res);
 });
 
-app.listen(8000);
+// passing express app to socketIO
+const server = app.listen(8000);
+const io = socketIO(server);
+
+// sockets
+io.on('connection', (client) => {
+  // send a message to the client every 5 sec
+  setInterval(() => {
+    client.emit('new_message', new Date());
+  }, 5000);
+});
