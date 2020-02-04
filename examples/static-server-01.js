f9ae2135bba048215366ee2e311dc9470889b4ee
alexalikiotis <alexalikiotis5@gmail.com>
2020-02-04 22:25:44 +0200

    Fixing MIME type bug

--- examples/static-server-01.js
+++ examples/static-server-01.js
@@ -15,7 +15,7 @@ const server = http.createServer((req, res) => {
       return;
     }
 
-    res.writeHead(200, { 'Content-Type': 'plain/text' });
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(data);
   });
 });
